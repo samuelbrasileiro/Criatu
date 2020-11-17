c522f4e7ce708ffae74a77c0a64eec32809299b2
Samuel <samuelbsantosn@gmail.com>
2020-11-17 14:29:04 -0300

    Adicionando descrições e lendo o preview

--- Criatu/ViewController.swift
+++ Criatu/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let handler = FirebaseHandler()
+    //let handler = FirebaseHandler()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let mjks = "900032829"
+        
+        
+        Song.fetch(songID: mjks){result in
+            if case .success(let song) = result {
+                print(song.data![0].attributes!.artistName!)
+            }
+            else if case .failure(let error) = result{
+                print(error.localizedDescription)
+            }
+            
+        }
     }
 
 
