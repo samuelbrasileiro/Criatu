ef769202f59c279382df1ee5cad131f9e56b4d2c
Alanis <alanislimasantaclara00@gmail.com>
2020-12-04 09:00:27 -0300

    finalizando tela de perfil

--- Criatu/Extra/Keys.swift
+++ Criatu/Extra/Keys.swift
@@ -11,4 +11,6 @@ class Keys{
     static let kAPMAccessToken = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlNGUkM1TTQ3WjQifQ.eyJpYXQiOjE2MDU2MjcxNTQsImV4cCI6MTYyMTE3OTE1NCwiaXNzIjoiUDYzOTUyTVM3TCJ9.vnrDu0iFK8t7lU3T2ejCB7JCGm0cIFSf67bo8ld4Bp9wg12Wzap8somkPSrMx1Aw3GSW_CXR3t-l5m3WGLuTVg"
     
     static let kSelectedInterests = "selected-interests-key"
+    
+    static let kPaletteMainColor = "pallete-main-color-key"
 }
--- Criatu/Profile/ProfileView.swift
+++ Criatu/Profile/ProfileView.swift
@@ -7,21 +7,59 @@
 
 import SwiftUI
 
+class Palette: ObservableObject{
+    static var shared: Palette = Palette()
+    
+    let colors: [Color] = [.systemPurple, .systemBlue, .systemRed, .systemGreen, .systemPink, .systemTeal, .systemOrange, .systemIndigo, .systemYellow].map{Color($0)}
+    
+    let descriptions: [String] = ["Roxo Elétrico", "Azul Pacífico","Vermelho Melancia", "Verde Amoeba", "Rosa Choque", "Ciano Tiffany", "Laranja Laranja", "Roxo Duquesa", "Amarelo Night"]
+    
+    var selectedMainColor: Int = 0{
+        didSet{
+            main = colors[selectedMainColor]
+            archive(color: selectedMainColor, for: Keys.kPaletteMainColor)
+        }
+    }
+    
+    @Published var main: Color
+    
+    private init(){
+        
+        selectedMainColor = Palette.restore(for: Keys.kPaletteMainColor)
+        
+        self.main = colors[selectedMainColor]
+    }
+    
+    func archive(color: Int, for key: String){
+        let defaults = UserDefaults(suiteName: "group.ufpe.academy.criatu")!
+        
+        defaults.setValue(color, forKey: key)
+        print("Changed color to ", colors[color])
+        
+        
+    }
+    
+    class func restore(for key: String) -> Int {
+        let defaults = UserDefaults(suiteName: "group.ufpe.academy.criatu")!
+        let color = defaults.integer(forKey: key)
+        print(color)
+        return color
+    }
+    
+}
 
 
 struct ProfileView: View {
     
     
-    let colors: [Color] = [Color(.systemPurple), Color(.systemBlue), Color(.systemRed), Color(.systemGreen)]
-    let descriptionText: [String] = ["Roxo Elétrico", "Ciano Sombrio","Vermelho Melancia","Verde Amoeba"]
-    
-    
-    var name: String
-    var wardrobe: String
+    var name: String = "Salumalatrix"
+    var wardrobe: Int = 4
     
     @State var selectedIndex = 0
     @State var showNotification = true
     @State var changeColors = true
+    
+    @ObservedObject var palette = Palette.shared
     
     var body: some View {
         VStack{
@@ -29,17 +67,50 @@ struct ProfileView: View {
             Text("Olá, " + name)
                 .font(.title)
                 .fontWeight(.semibold)
-                .foregroundColor(Color.purple)
+                .foregroundColor(palette.main)
                 .padding(.bottom, 10.0)
             
             
             Text("Até o momento você já descobriu ")
                 .padding(10.0)
+    
             
-            Text(wardrobe)
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .padding(20.0)
+            ZStack{
+                Circle()
+                    .frame(width: 40, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(palette.main)
+                    .opacity(0.6)
+                    .position(x: 70, y: 60)
+                Circle()
+                    .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(palette.main)
+                    .opacity(0.8)
+                    .position(x: 20, y: 30)
+                Circle()
+                    .frame(width: 30, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(palette.main)
+                    .opacity(0.8)
+                    .position(x: 70, y: 20)
+                
+                Circle()
+                    .frame(width: 30, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(palette.main)
+                    .opacity(0.8)
+                    .position(x: 15, y: 80)
+                Circle()
+                    .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(palette.main)
+                    .opacity(0.4)
+                    .position(x: 47, y: 85)
+                
+                Text(String(wardrobe))
+                    .font(.system(size: 48, weight: .bold, design: .default))
+                    .foregroundColor(.white)
+                    .shadow(radius: 3)
+                    .padding(20.0)
+                
+            } .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+
             
             Text("Armários")
                 .font(.headline)
@@ -55,62 +126,50 @@ struct ProfileView: View {
                     .cornerRadius(40, corners: [.topLeft, .topRight])
                     .shadow(radius: 15).opacity(0.9)
                 
-                VStack(alignment: .leading){
+                VStack(alignment: .leading, spacing: 5){
                     Text("Cor do tema")
                         .font(.title3)
                         .fontWeight(.light)
                         .multilineTextAlignment(.leading)
                         .padding()
-                    //                        .position(x: 80, y: 55)
-                    
-                    HStack{
-                        ForEach(0..<colors.count, id: \.self) { index in
+                    ScrollView(.horizontal){
+                        LazyHStack(spacing: 15){
+                            ForEach(0..<palette.colors.count, id: \.self) { index in
+                                
+                                Button(action: {
+                                    selectedIndex = index
+                                    palette.selectedMainColor = selectedIndex
+                                }, label: {
+                                    if selectedIndex == index{
+                                        VStack(){
+                                            Circle()
+                                                .foregroundColor(palette.colors[index])
+                                                
+                                                .frame(width: 40, height: 40, alignment: .center)
+                                                .overlay(
+                                                    RoundedRectangle(cornerRadius: 20)
+                                                        .stroke(palette.colors[index], lineWidth: 5)
+                                                )
+                                                .overlay(
+                                                    RoundedRectangle(cornerRadius: 20)
+                                                        .stroke(Color(.systemGray6), lineWidth: 1)
+                                                )
+                                        }
+                                    }
+                                    else{
+                                        VStack{
+                                            Circle().foregroundColor(palette.colors[index])
+                                                .frame(width: 40, height: 40, alignment: .center)
+                                            
+                                        }
+                                    }
+                                })
+                            }
                             
-                            Button(action: {
-                                selectedIndex = index
-                            }, label: {
-                                if selectedIndex == index{
-                                    
-                                    
-                                    VStack(){
-                                        Circle()
-                                            .foregroundColor(colors[index])
-                                            //                                            .border(Color.black, width: 2)
-                                            .frame(width: 40, height: 40, alignment: .center)
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 20)
-                                                    .stroke(colors[index], lineWidth: 5)
-                                            )
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 20)
-                                                    .stroke(Color(.systemGray6), lineWidth: 1)
-                                            )
-                                        
-                                        
-                                        
-                                        
-                                    }
-                                    //                        ZStack{
-                                    //                            Circle().foregroundColor(.black)
-                                    //                                .frame(width: 50, height: 50, alignment: .center)
-                                    //                        }
-                                }
-                                else{
-                                    
-                                    
-                                    
-                                    VStack{
-                                        Circle().foregroundColor(colors[index])
-                                            .frame(width: 40, height: 40, alignment: .center)
-                                        
-                                    }
-                                }
-                            })
-                        }
-                        
-                    }.padding()
-                    
-                    Text("\(descriptionText[selectedIndex])" + " é o seu tema no momento")
+                        }.padding()
+                    }
+                    .frame(height: 60)
+                    Text("\(palette.descriptions[selectedIndex])" + " é o seu tema no momento")
                         .font(.footnote)
                         .fontWeight(.thin)
                         .italic()
@@ -118,34 +177,46 @@ struct ProfileView: View {
                         .foregroundColor(Color(.systemGray))
                         .lineLimit(1)
                     
-                    
+                    Divider()
+
                     VStack {
                         Toggle(isOn: $showNotification) {
                             Text("Notificações")
-                        }.padding()
+                        }
+                        .toggleStyle(SwitchToggleStyle(tint: palette.main))
+                        
+                        .padding()
                         
                         if showNotification {
                         }
                     }
+                    Divider()
                     
                     VStack() {
-                        
-                        Text("Interesses")
+                        HStack{
+                            Text("Interesses")
+                            Spacer()
+                            Image(systemName: "chevron.forward")
+                                .foregroundColor(palette.main)
+                        }.padding()
                         //ver o navigationlink aqui
-                    }.padding()
+                    }
                     
+                    Divider()
                     
-                    
-                    
+                    Spacer()
                     
                 }.frame(minWidth: 200, idealWidth: 300, maxWidth: .infinity, minHeight: 250, idealHeight: 400, maxHeight: 500, alignment: .center)
             }
+            .padding()
         }
+        .animation(.spring())
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileView(name: "Salumalatrix", wardrobe: "4")
+        ProfileView(name: "Salumalatrix", wardrobe: 4)
     }
 }
+
